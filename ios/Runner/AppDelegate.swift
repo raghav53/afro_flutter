3d295d71eaa9d29d2a283b22f40714a76fe97d48
Pankaj Sharma <smartparadise713@gmail.com>
2022-12-19 11:43:46 +0530

    ios fb, google setup

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import UIKit
 import Flutter
+import Firebase
+import UserNotifications
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +9,16 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+          let token = deviceToken.map{String(format: "%02.2hhx", $0)}.joined()
+          print("pushTokenFCM_ios ",token)
+          Messaging.messaging().apnsToken = deviceToken
+          
+          super.application(application,
+                            didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+      }
 }
